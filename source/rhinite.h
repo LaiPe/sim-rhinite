8f603d9d3c00816d7ae8c32b0f5deab6195b344c
Léo Desktop <peyronnet.leo@gmail.com>
2024-06-14 16:40:16 +0200

    UPDATE: rhinite.h

--- source/rhinite.h
+++ source/rhinite.h
@@ -14,8 +14,8 @@ typedef struct {
 
 personne_t * init_population(int num_personnes, int taille_grille);
 
-void deplacement_alea(personne_t * population, int num_personnes, int taille_grille);
-
 void init_contamination(personne_t * population, int nb_initial_contamines, int num_personnes);
+
+int * launch_sim(int nb_jours, personne_t * population, int taille_grille, int num_personnes, int duree_incubation, int duree_contagion, int duree_imunitee, double * proba_contamination);
 
 #endif
