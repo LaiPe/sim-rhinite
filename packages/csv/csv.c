9bc82585d88ad5476e152920802848b166c92e62
Léo Desktop <peyronnet.leo@gmail.com>
2024-06-15 12:14:25 +0200

    UPDATE: package csv (write_double_matrix_CSV)

--- packages/csv/csv.c
+++ packages/csv/csv.c
@@ -5,7 +5,7 @@
 
 #include "./csv.h"
 
-void write_int_array_CSV(const char *filename, int *array, int size)
+void write_int_array_CSV(const char * filename, int * array, int size)
 {
     // Création du répertoire ./out s'il n'existe pas
     struct stat st = {0};
@@ -26,11 +26,48 @@ void write_int_array_CSV(const char *filename, int *array, int size)
         return;
     }
     
+    // Écriture
     for (int i = 0; i < size; i++) {
         fprintf(file, "%d", array[i]);
         if (i < size - 1) {
             fprintf(file, ",");
         }
+    }
+    
+    fclose(file);
+}
+
+
+void write_double_matrix_CSV(const char *filename, double **array, int size_x, int size_y)
+{
+    // Création du répertoire ./out s'il n'existe pas
+    struct stat st = {0};
+    if (stat("./out", &st) == -1) {
+        if (mkdir("./out", 0700) != 0) {
+            perror("Error creating directory");
+            return;
+        }
+    }
+
+    // Construction du chemin complet du fichier
+    char filepath[256];
+    snprintf(filepath, sizeof(filepath), "./out/%s", filename);
+
+    FILE *file = fopen(filepath, "w");
+    if (file == NULL) {
+        perror("Error opening file");
+        return;
+    }
+    
+    // Écriture
+    for (int i = 0; i < size_x; i++) {
+        for (int j = 0; j < size_y; j++) {
+            fprintf(file, "%f", array[i][j]);
+            if (j < size_y - 1) {
+                fprintf(file, ",");
+            }
+        }
+        fprintf(file, "\n");
     }
     
     fclose(file);
--- packages/csv/csv.h
+++ packages/csv/csv.h
@@ -3,4 +3,6 @@
 
 void write_int_array_CSV(const char *filename, int *array, int size);
 
+void write_double_matrix_CSV(const char * filename, double ** array, int size_x, int size_y);
+
 #endif
